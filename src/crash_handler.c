cdf309cf1b2d9205e106565ea4e953af9a73348a
NeGate <yasserarguelles2506@gmail.com>
2022-05-22 02:10:44 -0400

    Switched to smaller crash dumps on Windows

--- src/crash_handler.c
+++ src/crash_handler.c
@@ -13,19 +13,19 @@ static mtx_t crash_mutex;
 static LONG WINAPI unhandled_exception_handler(PEXCEPTION_POINTERS exception_ptrs) {
 	mtx_lock(&crash_mutex);
 	time_t now = time(NULL);
-    
+
 	char path[_MAX_PATH + 1];
 	sprintf_s(path, _MAX_PATH + 1, "./crash_dump_%lld.dmp", now);
-	
+
     char* new_path = malloc(MAX_PATH);
 	if (GetFullPathNameA(path, MAX_PATH, new_path, NULL) == 0) {
 		printf("GetFullPathNameA broke while in the middle of a crash... yikes?");
 		return EXCEPTION_EXECUTE_HANDLER;
 	}
-	
+
 	printf("A crash happened, please make a Github Issue or something i dont control you :p\n");
 	printf("%s\n\n", new_path);
-	
+
 	// This writes a dump file to the current dir.
 	// To view this file open it up in Visual Studio
 	// If source and symbols are available you should
@@ -37,25 +37,21 @@ static LONG WINAPI unhandled_exception_handler(PEXCEPTION_POINTERS exception_ptr
 										 CREATE_ALWAYS,
 										 FILE_ATTRIBUTE_NORMAL,
 										 NULL);
-	
+
 	MINIDUMP_EXCEPTION_INFORMATION mini_dump_info = {
 		.ThreadId = GetCurrentThreadId(),
 		.ExceptionPointers = exception_ptrs,
 		.ClientPointers = TRUE
 	};
-	
+
 	MiniDumpWriteDump(GetCurrentProcess(),
 					  GetCurrentProcessId(),
 					  crash_dump_file,
-					  (MINIDUMP_TYPE)(MiniDumpWithHandleData |
-						  			MiniDumpWithFullMemory |
-									MiniDumpWithProcessThreadData |
-									MiniDumpWithFullMemoryInfo |
-									MiniDumpWithThreadInfo),
+					  (MINIDUMP_TYPE)(MiniDumpWithProcessThreadData | MiniDumpWithThreadInfo),
 					  &mini_dump_info,
 					  NULL,
 					  NULL);
-	
+
 	CloseHandle(crash_dump_file);
 	ExitProcess(69420);
 }
@@ -68,6 +64,6 @@ void hook_crash_handler() {
 #else
 // #error "Implement crash handler on this platform"
 void hook_crash_handler() {
-	
+
 }
 #endif
