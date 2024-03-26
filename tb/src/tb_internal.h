105588e5e1ebf0a4220bbb47b37f335511648851
Yasser Arguelles Snape <yasserarguelles2506@gmail.com>
2024-03-26 12:41:50 -0700

    Update tb_internal.h

--- tb/src/tb_internal.h
+++ tb/src/tb_internal.h
@@ -65,7 +65,7 @@ for (uint64_t _bits_ = (bits), it = (start); _bits_; _bits_ >>= 1, ++it) if (_bi
 ////////////////////////////////
 #define TB_OPTDEBUG_STATS    0
 #define TB_OPTDEBUG_PASSES   0
-#define TB_OPTDEBUG_PEEP     1
+#define TB_OPTDEBUG_PEEP     0
 #define TB_OPTDEBUG_SCCP     0
 #define TB_OPTDEBUG_LOOP     0
 #define TB_OPTDEBUG_SROA     0
