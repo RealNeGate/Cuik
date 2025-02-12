5576b91910b9fb0e0f9fd86f9a10fc37334f2f79
Yasser Arguelles Snape <yasserarguelles2506@gmail.com>
2025-02-11 22:59:55 -0800

    Update foo.c

--- foo.c
+++ foo.c
@@ -2,9 +2,21 @@
 
 void matmul(float* dst, float* a, float* b);
 
-static float a[] = { 1.0, 0.0, 0.0, 1.0 };
-static float b[] = { 0.5, 0.0, 0.0, 0.5 };
-static float dst[4];
+static float a[] = {
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0
+};
+
+static float b[] = {
+    0.5, 0.0, 0.0, 4.0,
+    0.0, 0.5, 0.0, 3.0,
+    0.0, 0.0, 0.5, 2.0,
+    0.0, 0.0, 0.0, 1.0
+};
+
+static float dst[16];
 
 int main() {
     matmul(dst, a, b);
