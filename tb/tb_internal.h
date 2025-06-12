0ee5925efb2fd35f2b5cc42e48a3ead53511a9df
Yasser Arguelles Snape <yasserarguelles2506@gmail.com>
2025-06-11 23:36:01 -0700

    separated config out

--- tb/tb_internal.h
+++ tb/tb_internal.h
@@ -18,36 +18,7 @@
 #define thread_local _Thread_local
 #endif
 
-////////////////////////////////
-// Random toggles
-////////////////////////////////
-#define TB_OPTDEBUG_STATS     0
-#define TB_OPTDEBUG_PASSES    1
-#define TB_OPTDEBUG_PEEP      0
-#define TB_OPTDEBUG_SCCP      0
-#define TB_OPTDEBUG_LOOP      0
-#define TB_OPTDEBUG_SROA      0
-#define TB_OPTDEBUG_GCM       0
-#define TB_OPTDEBUG_SLP       0
-#define TB_OPTDEBUG_GVN       0
-#define TB_OPTDEBUG_INTERP    0
-#define TB_OPTDEBUG_MEM2REG   0
-#define TB_OPTDEBUG_ISEL      0
-#define TB_OPTDEBUG_ISEL2     0
-#define TB_OPTDEBUG_ISEL3     0
-#define TB_OPTDEBUG_EMIT      0
-#define TB_OPTDEBUG_DATAFLOW  0
-#define TB_OPTDEBUG_PLACEMENT 0
-#define TB_OPTDEBUG_INLINE    0
-#define TB_OPTDEBUG_REGALLOC  1
-#define TB_OPTDEBUG_REGALLOC2 0
-#define TB_OPTDEBUG_REGALLOC3 1
-#define TB_OPTDEBUG_REGALLOC4 0
-#define TB_OPTDEBUG_COMPACT   0
-#define TB_OPTDEBUG_SCHED1    0
-#define TB_OPTDEBUG_SCHED2    0
-// for toggling ANSI colors
-#define TB_OPTDEBUG_ANSI      1
+#include "tb_config.h"
 
 #define TB_OPTDEBUG(cond) CONCAT(DO_IF_, CONCAT(TB_OPTDEBUG_, cond))
 
